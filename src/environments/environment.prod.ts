236b13d62c9086b2f9340479610d111bb3bc40dc
thaind <nguyenduythai12@gmail.com>
2019-12-08 09:36:24 +0700

    fix table

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,4 +5,14 @@
  */
 export const environment = {
   production: true,
+  firebase: {
+    apiKey: 'AIzaSyAwBHuUl1uKusikMB9hGJZ74lBa6RTMbcU',
+    authDomain: 'cafe-clone.firebaseapp.com',
+    databaseURL: 'https://cafe-clone.firebaseio.com',
+    projectId: 'cafe-clone',
+    storageBucket: 'cafe-clone.appspot.com',
+    messagingSenderId: '838585538343',
+    appId: '1:838585538343:web:f75df87e732d986e857b02',
+    measurementId: 'G-NVR3R6T89Y',
+  },
 };
